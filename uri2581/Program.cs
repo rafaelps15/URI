b2b01fe993ca4a050f033647df2f2565881d5234
Rafael Santos <75229214+rafaelps15@users.noreply.github.com>
2022-05-09 00:54:08 -0300

    Update Program.cs
    Corrigida a especificação do exercício.

--- uri2581/Program.cs
+++ uri2581/Program.cs
@@ -2,8 +2,10 @@
 
 namespace uri2581
 {
-    /*Toorg é o integrante mais sábio do grupo de heróis denominado Os Protetores da Via Láctea. Para qualquer pergunta,
-    ele tem a resposta ideal! Escreva um programa que, dada uma pergunta, informe a resposta de Toorg.
+    /*Toorg é o integrante mais sábio do grupo de heróis denominado Os Protetores da Via Láctea. 
+    Para qualquer pergunta, ele tem a resposta ideal!
+    Escreva um programa que, dada uma pergunta, informe a resposta de Toorg.
+    
     Entrada:
     A entrada terá diversos casos de teste.A cada caso de teste, um número N é apresentado, que representa o número de casos de teste.
     Em seguida, haverá N linhas, com as perguntas feitas para Toorg.
