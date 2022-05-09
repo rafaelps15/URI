4fc887db955b366852fb453cded1fc88dab631a3
Rafael Santos <75229214+rafaelps15@users.noreply.github.com>
2022-05-09 00:55:34 -0300

    Update Program.cs
    Corrigida a especificação do exercício.

--- uri1000/Program.cs
+++ uri1000/Program.cs
@@ -3,8 +3,10 @@
 namespace uri1000
 {
     /*Bem-vindo ao beecrowd!
+    
     O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". 
-    Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.*/
+    Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+    */
 
     class Program
     {
