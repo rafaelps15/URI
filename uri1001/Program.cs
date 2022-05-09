8757fbfc53fea82178c8b5f02f2a0495caf4e699
Rafael Santos <75229214+rafaelps15@users.noreply.github.com>
2022-05-09 02:04:34 -0300

    Update Program.cs
    Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X. Imprima X conforme exemplo apresentado abaixo. Não apresente mensagem alguma além daquilo que está sendo especificado e não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".

--- uri1001/Program.cs
+++ uri1001/Program.cs
@@ -2,6 +2,13 @@
 
 namespace uri1001
 {
+  /*Leia 2 valores inteiros e armazene-os nas variáveis A e B. 
+    Efetue a soma de A e B atribuindo o seu resultado na variável X. 
+      
+    Imprima X conforme exemplo apresentado abaixo. 
+    Não apresente mensagem alguma além daquilo que está sendo especificado e não esqueça de imprimir
+    o fim de linha após o resultado,caso contrário, vocêreceberá "Presentation Error".*/
+        
     class Program
     {
         static void Main(string[] args)
