015b5ca0467ea5ba6cfca023e0641d241dab7c28
Rafael Santos <75229214+rafaelps15@users.noreply.github.com>
2022-05-09 00:39:01 -0300

    Corrigido
    Corrigida a descrição do exercício

--- uri1000/Program.cs
+++ uri1000/Program.cs
@@ -2,6 +2,10 @@
 
 namespace uri1000
 {
+    /*Bem-vindo ao beecrowd!
+    O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". 
+    Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.*/
+
     class Program
     {
         static void Main(string[] args)
