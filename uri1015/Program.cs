f3dff68fa9beffcd1cb757ccdd5654c536eb7125
Rafael Santos <75229214+rafaelps15@users.noreply.github.com>
2022-05-11 04:40:55 -0300

    Update Program.cs

--- uri1015/Program.cs
+++ uri1015/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Globalization;
 
 namespace uri1015
@@ -7,6 +7,15 @@ namespace uri1015
     {
         static void Main(string[] args)
         {
+            /*Leia os quatro valores correspondentes aos eixos x e y de dois pontos quaisquer no plano, p1(x1, y1) e p2(x2, y2) e calcule
+              a distância entre eles, mostrando 4 casas decimais após a vírgula, segundo a fórmula:
+
+              Distancia = Raiz(x2 - x1)² + (y2 - y1)²:
+
+              Entrada
+              O arquivo de entrada contém duas linhas de dados.A primeira linha contém dois valores de ponto flutuante: x1 y1 e a segunda 
+              linha contém dois valores de ponto flutuante x2 y2.
+            */
             double x1, x2, y1, y2, distancia;
 
             string[] valores = Console.ReadLine().Split(' ');
@@ -21,7 +30,6 @@ namespace uri1015
             distancia = Math.Sqrt(Math.Pow(x2 - x1, 2.0) + (Math.Pow(y2 - y1, 2.0)));
 
             Console.WriteLine(distancia.ToString("F4", CultureInfo.InvariantCulture));
-
         }
     }
 }
