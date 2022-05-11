04bb75f3dd07a883ff1a153820dea38805a81819
Rafael Santos <75229214+rafaelps15@users.noreply.github.com>
2022-05-11 04:34:35 -0300

    Update Program.cs

--- uri1007/Program.cs
+++ uri1007/Program.cs
@@ -1,9 +1,18 @@
-﻿using System;
+using System;
 
 namespace uri1007
 {
     class Program
     {
+        /*Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: 
+          DIFERENCA = (A * B - C * D).
+
+          Entrada
+          O arquivo de entrada contém 4 valores inteiros.
+
+          Saída
+          Imprima a mensagem DIFERENCA com todas as letras maiúsculas, conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade.
+        */
         static void Main(string[] args)
         {
             int a, b, c, d, Diferencaproduto;
@@ -16,7 +25,6 @@ namespace uri1007
             Diferencaproduto = (a * b) - (c * d);
 
             Console.WriteLine("DIFERENCA = " + Diferencaproduto);
-
         }
     }
 }
