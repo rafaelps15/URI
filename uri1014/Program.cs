8d24cab4104c4ac91fad54a034eeb9a0f237e44b
Rafael Santos <75229214+rafaelps15@users.noreply.github.com>
2022-05-11 04:40:12 -0300

    Update Program.cs

--- uri1014/Program.cs
+++ uri1014/Program.cs
@@ -1,10 +1,19 @@
-﻿using System;
+using System;
 using System.Globalization;
 
 namespace uri1014
 {
     class Program
     {
+        /*Calcule o consumo médio de um automóvel sendo fornecidos a distância total percorrida (em Km) e o total de combustível gasto (em litros).
+
+          Entrada
+          O arquivo de entrada contém dois valores: um valor inteiro X representando a distância total percorrida (em Km), 
+          e um valor real Y representando o total de combustível gasto, com um dígito após o ponto decimal.
+
+          Saída
+          Apresente o valor que representa o consumo médio do automóvel com 3 casas após a vírgula, seguido da mensagem "km/l".
+         */
         static void Main(string[] args)
         {
             int x;
