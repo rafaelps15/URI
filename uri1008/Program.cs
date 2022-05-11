a23e581bfe0967af840f93d812bd5352a603fa33
Rafael Santos <75229214+rafaelps15@users.noreply.github.com>
2022-05-11 04:36:26 -0300

    Update Program.cs

--- uri1008/Program.cs
+++ uri1008/Program.cs
@@ -1,9 +1,20 @@
-﻿using System;
+using System;
 using System.Globalization;
 namespace uri1008
 {
     class Program
     {
+        /*Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora
+          e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
+
+          Entrada
+          O arquivo de entrada contém 2 números inteiros e 1 número com duas casas decimais, representando o número, quantidade
+          de horas trabalhadas e o valor que o funcionário recebe por hora trabalhada, respectivamente.
+
+          Saída
+          Imprima o número e o salário do funcionário, conforme exemplo fornecido, com um espaço em branco antes e depois da 
+          igualdade. No caso do salário, também deve haver um espaço em branco após o $.
+        */
         static void Main(string[] args)
         {
             int numFunc, hrTrab;
@@ -16,7 +27,7 @@ namespace uri1008
             salarioFunc = hrTrab * valorHr;
 
             Console.WriteLine("NUMBER = " + numFunc);
-            Console.WriteLine("SALARY = U$ " + salarioFunc.ToString("F2"), CultureInfo.InvariantCulture);             
+            Console.WriteLine("SALARY = U$ " + salarioFunc.ToString("F2"), CultureInfo.InvariantCulture);
         }
     }
 }
