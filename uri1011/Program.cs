89720ba90118f6347eca2549742a4b7b79dd50e6
Rafael Santos <75229214+rafaelps15@users.noreply.github.com>
2022-05-11 04:38:12 -0300

    Update Program.cs

--- uri1011/Program.cs
+++ uri1011/Program.cs
@@ -1,10 +1,23 @@
-﻿using System;
+using System;
 using System.Globalization;
 
 namespace uri1011
 {
     class Program
     {
+        /*Faça um programa que calcule e mostre o volume de uma esfera sendo fornecido o valor de seu raio (R). 
+          A fórmula para calcular o volume é: (4/3) * pi * R3. Considere (atribua) para pi o valor 3.14159.
+
+          Dica: Ao utilizar a fórmula, procure usar (4/3.0) ou (4.0/3), pois algumas linguagens (dentre elas o C++), 
+          assumem que o resultado da divisão entre dois inteiros é outro inteiro.
+
+          Entrada
+          O arquivo de entrada contém um valor de ponto flutuante (dupla precisão), correspondente ao raio da esfera.
+
+          Saída
+          A saída deverá ser uma mensagem "VOLUME" conforme o exemplo fornecido abaixo, com um espaço antes e um espaço
+          depois da igualdade. O valor deverá ser apresentado com 3 casas após o ponto.
+        */
         static void Main(string[] args)
         {
             double volume, raio, pi = 3.14159;
@@ -14,7 +27,6 @@ namespace uri1011
             volume = 4.0 / 3 * pi * (raio * raio * raio);
 
             Console.WriteLine("VOLUME = " + volume.ToString("F3", CultureInfo.InvariantCulture));
-
         }
     }
 }
