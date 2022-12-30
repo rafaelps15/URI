d9089bd1040235cfab89b95f348a73c7bb1562a3
Rafael <75229214+rafaelps15@users.noreply.github.com>
2022-12-30 19:48:38 -0300

    final resolution

--- uri1000/Program.cs
+++ uri1000/Program.cs
@@ -7,7 +7,6 @@ namespace uri1000
     O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". 
     Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
     */
-
     class Program
     {
         static void Main(string[] args)
